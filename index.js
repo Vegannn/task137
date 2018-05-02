fe153cc2689e9ffec9fe6e56aa66b24381a6a71d
Rafal Stefaniak <vegecia@gmail.com>
2018-05-03 01:23:57 +0200

    Poprawki od Michala

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var colors = require('colors');
 fs.readdir('.', function(err,data){
 	console.log('Dane przed zapisem!'.green);
 	console.log(data);
-	fs.writeFile('./strukturafolderu.txt', 'A tak wyglądają po zapisie!', function(err){
+	fs.writeFile('./strukturafolderu.txt', data.join('\n'), function(err){
 		if (err) throw err;
 		console.log('Zapisano!'.green);
 		fs.readFile('./strukturafolderu.txt', 'utf-8', function(err, data) {
